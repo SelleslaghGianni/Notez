16c0e95e729f91906bb827ce6f646108c553576c
Gianni Selleslagh <selleslagh.gianni@gmail.com>
2021-09-16 17:11:49 +0200

    Base code from fullstackopen.com and added my own flavor

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
 
-function App() {
+const Note = ({ note }) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <li>{note.content}</li>
+  )
+}
+
+const App = (props) => {
+  const [notes, setNotes] = useState(props.notes)
+  const [newNote, setNewNote] = useState('')
+
+  const addNote = (event) => {
+    event.preventDefault()
+    const noteObject = {
+      content: newNote,
+      id: notes.length + 1,
+      deleted: false
+    }
+
+    setNotes(notes.concat(noteObject))
+    setNewNote('')
+  }
+  function handleDeleteNote (id) {
+    const newNotes = notes.filter(item => item.id !== id )
+    setNotes(newNotes)
+  }
+
+  const handleNoteChange = (event) => {
+    setNewNote(event.target.value)
+  }
+
+
+  return (
+    <div>
+      <h1 className="text-6xl font-extrabold">Notez</h1>
+      <div>
+        <ul>
+          {notes.map(note =>  
+              [<Note key={note.id} note={note}/>,
+              <button key={note.id+500}type="button" 
+                onClick={() => handleDeleteNote(note.id)}>
+                x
+              </button>]
+          )}
+        </ul>
+      </div>
+      <form onSubmit={addNote}>
+        <input 
+          value={newNote}
+          onChange={handleNoteChange}  
+        />
+        <button type="submit">save</button>
+      </form>
     </div>
-  );
+  )
 }
 
 export default App;
